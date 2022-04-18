145fd2b45b1e8652c0aa571900fa2b42dd4cefb0
Lester <lestradae6@miumg.edu.gt>
2022-04-17 19:40:49 -0600

    V1

---
+++ Crud_Marca.cpp
@@ -0,0 +1,223 @@
+#include<iostream>
+#include<mysql.h>
+#include<mysql_error.h>
+#include<locale.h>
+
+using namespace std;
+	class clempresa{
+		
+		public:
+		MYSQL *obj;
+		
+		char* conectar(conectar,"localhost","usr_empresa", "Empres@123", "db_empresa,marca", 3306, NULL,0 ){
+			
+				if(!(obj=mysql_init(0)))
+					{
+						return "Error Create Objeto";
+					}
+					if(!mysql_real_connect("localhost","usr_empresa", "Empres@123", "db_empresa.marca", 3306, NULL,0 ) )
+					{
+						return (char*)mysql_error(obj);
+					}else
+					{
+						return "==========>> Base de Datos en Uso <<==========";
+					}
+		}
+		
+		void desconectar(){
+			mysql_close(obj);
+		}
+	};
+	
+	conne conexion;
+	
+	struct stempresa{
+	char idmarca[6];
+	char marcas[20];
+	
+	}clempresa;
+	
+	class clempresa{
+		public:
+			char *resultado;
+			void insertar(struct stempresa ){
+				try{
+				conexion.conectar("localhost","usr_empresa", "Empres@123", "db_empresa.marca");
+				char *consulta;
+				char sentencia[]="INSERT INTO  db_empresa(ID,NOMBRE,PATERNO) VALUES (\'%s\',\'%s\',\'%s\')";
+				consulta=new char[strlen(sentencia)+sizeof(stmarca)]-sizeof(int);
+				sprintf(consulta,sentencia,id.marca, marcas);
+				int aux=mysql_query(conexion.obj,consulta);
+				if(!aux==0){
+					throw mysql_error(conexion.obj);
+				}
+				resultado="Registro Ingresado Correctamente\n";
+				}catch(const char* msg){
+					cout<<msg;
+					conexion.desconectar();
+				}
+			}
+			
+			void modificar(struct stempresa){
+				try{
+				conexion.conectar("localhost","usr_empresa", "Empres@123", "db_empresa.marca";
+				char *consulta;
+				char sentencia[]="UPDATE ALUMNO SET NOMBRE=\'%s\',PATERNO=\'%s\' wHERE ID=\'%s\'";
+				consulta=new char[strlen(sentencia)+sizeof(stempresa)]-sizeof(int);
+				sprintf(consulta,sentencia,id.marca, marcas);
+				int aux=mysql_query(conexion.obj,consulta);
+				if(!aux==0){
+					throw mysql_error(conexion.obj);
+				}
+				resultado="Registro Modificado\n";
+				}catch(const char* msg){
+					cout<<msg;
+					conexion.desconectar();
+				}
+			}
+			
+			void eliminar(struct stempresa){
+				try{
+				conexion.conectar("localhost","usr_empresa", "Empres@123", "db_empresa.marca");
+				char *consulta;
+				char sentencia[]="DELETE from marca WHERE ID=\'%s\'";
+				consulta=new char[strlen(sentencia)+sizeof(stempresa)]-sizeof(int);
+				sprintf(consulta,sentencia,marca.id);
+				int aux=mysql_query(conexion.obj,consulta);
+				if(!aux==0){
+					throw mysql_error(conexion.obj);
+				}
+				resultado="Registro ELiminado Correctamente\n";
+				}catch(const char* msg){
+					cout<<msg;
+					conexion.desconectar();
+				}
+			}
+			
+			void mostrar(){
+				   MYSQL           *objDatos;
+				   MYSQL_RES       *res;
+				   MYSQL_ROW        row;
+				   MYSQL_FIELD     *columna;
+				   int              i, j, k, l;
+				   unsigned long   *lon;
+				   MYSQL_ROW_OFFSET pos;
+				   if(!(objDatos = mysql_init(0))) {
+				      cout<<"Error Objeto"<<endl;
+				      rewind(stdin);
+				      getchar();	      
+					}
+				   if(!mysql_real_connect(objDatos,"localhost", "usr_empresa", "Empres@123", "db_empresa.marca", 3306, NULL, 0)) {
+				      cout<<"Error BD"<<MYSQL_PORT<<" Error: "<<mysql_error(objDatos)<<endl;
+				      mysql_close(objDatos);
+				      rewind(stdin);
+				      getchar();
+				   }
+				   if(mysql_select_db(objDatos, "db_empresa.marca")) {
+				      cout<<mysql_error(objDatos)<<endl;
+				      mysql_close(objDatos);
+				      rewind(stdin);
+				      getchar();
+				   }
+				   if(mysql_query(objDatos, "SELECT * FROM marca")) {
+				      cout<<mysql_error(objDatos)<<endl;
+				      mysql_close(objDatos);
+				      rewind(stdin);
+				      getchar();
+				   }
+				if((res=mysql_store_result(objDatos))){
+					i=(int)mysql_num_rows(res);
+					j=(int)mysql_num_fields(res);
+					cout<<endl<<"		Datos Generales"<<endl;
+					l=1;
+					for(l=0;l<i;l++){
+						row=mysql_fetch_row(res);
+						lon=mysql_fetch_lengths(res);
+						cout<<endl;
+						for(k=0;k<j;k++){
+						cout<<"	"<<((row[k]==NULL)?"NULL":row[k])<<"	";
+					}
+				}cout<<endl;
+			    mysql_free_result(res);
+			   }
+			   mysql_close(objDatos);
+			   rewind(stdin);
+			   cout<<endl;
+			}
+			
+	};
+	
+	void salir(){
+		cout<<"Programa Finalizado\n";
+	}
+	
+	void menu(){
+		int opcion;
+		clempresa op;
+		cout<<"Presentado por: Lester Estrada\n";
+		do{
+		cout<<"---------------------------------------------------------------\n";
+		cout<<"                  +------------------------+"<<endl;
+		cout<<"                  |          Menu          |"<<endl;
+		cout<<"                  +------------------------+"<<endl;
+		cout<<"                  | Seleccione una Opción  |"<<endl;
+		cout<<"                  | 1.- Crear Registros    |"<<endl;
+		cout<<"                  | 2.- Eliminar Registros |"<<endl;
+		cout<<"                  | 3.- Modificar Registros|"<<endl;
+		cout<<"                  | 4.- Ver Registros      |"<<endl;
+		cout<<"                  | 5.- Salir              |"<<endl;
+		cout<<"                  +------------------------+"<<endl;
+		cout<<"---------------------------------------------------------------"<<endl;
+		
+		clempresa;
+		cin>>opcion;
+		
+		switch(opcion){
+				case 1:		
+					cout<<"Ingrese Datos  \n";
+					cout<<"Ingrese IDmarcas : ";
+					cin>>id.marca;
+					cout<<"Ingrese marca: ";
+					cin>>marca;
+					op.insertar(alu);
+					cout<<op.resultado;
+				break;
+				
+				case 2:		
+					clempresa op;
+					cout<<"Ingrese Código  a Eliminar: ";
+					cin>>alu.id;
+					op.eliminar(alu);
+					cout<<op.resultado;
+				break;
+				
+				case 3:
+					cout<<"Modificar Datos \n";
+					cout<<"Ingrese ID : ";
+					cin>>alu.id;
+					cout<<"\nModificará Registros con ID "<<alu.id<<endl;
+					cout<<"\nIngrese Nuevo Nombre: ";
+					cin>>alu.nombre;
+					cout<<"Ingrese Nuevo Apellido Paterno: ";
+					cin>>alu.paterno;
+					op.modificar(alu);
+					cout<<op.resultado;
+				break;
+				
+				case 4:
+					op.mostrar();
+					
+				break;
+				case 5: salir();
+				break;
+				default:
+				cout<<"Opción Incorrecta\n";	
+		}
+	}while(opcion!=5);
+	}
+	
+int main() {
+	setlocale(LC_CTYPE,"Spanish");
+	menu();
+	return 0;
+}
---
+++ Empresa.sql
@@ -0,0 +1,81 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: usr_empresa
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `marcas`
+--
+
+DROP TABLE IF EXISTS `marcas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `marcas` (
+  `idMarcas` smallint NOT NULL,
+  `marca` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idMarcas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marcas`
+--
+
+LOCK TABLES `marcas` WRITE;
+/*!40000 ALTER TABLE `marcas` DISABLE KEYS */;
+INSERT INTO `marcas` VALUES (1,'Kingston'),(2,'HyperX'),(3,'sandiskr');
+/*!40000 ALTER TABLE `marcas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `idproductos` int NOT NULL,
+  `producto` varchar(50) DEFAULT NULL,
+  `idMarca` smallint DEFAULT NULL,
+  `Descripcion` varchar(100) DEFAULT NULL,
+  `precio_costo` decimal(8,2) DEFAULT NULL,
+  `precio_venta` decimal(8,2) DEFAULT NULL,
+  `existencia` int DEFAULT NULL,
+  `fecha_ingreso` datetime DEFAULT NULL,
+  PRIMARY KEY (`idproductos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,'usb',2201,'almacenamiento',45.00,85.00,90,'2021-04-11 00:00:00'),(2,'sd',2202,'almacenamiento',35.00,75.00,70,'2021-04-11 00:00:00'),(3,'ram',2203,'almacenamiento',25.00,95.00,25,'2021-04-11 00:00:00');
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-11 12:42:27
